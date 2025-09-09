812aeb9bb9eab0175b03fe0d16f4d48bfa265e13
Erick20851 <erickgithub49@gmail.com>
2025-09-09 20:14:40 -0300

    jh

---
+++ script.js
@@ -0,0 +1,5 @@
+<script src="function mostrarMensagem() {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.innerText = "Reconhecer o problema é o primeiro passo. Procure ajuda profissional e cuide do seu sono.";
+}
+"></script>
